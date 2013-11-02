f36d189c04156471f5a71b3e9aba53ad17eecd84
softcadbury <romain.parage@gmail.com>
2013-11-02 15:49:47 +0100

    - Update bolg pages

--- Softox/Scripts/script.js
+++ Softox/Scripts/script.js
@@ -53,7 +53,7 @@ function init_editor_coloration() {
         var content = $(this).html();
 
         var keywords = ['public ', 'void ', 'static ', 'return', 'delegate ', 'event', 'Func', 'Action',
-                        'Object', 'string', 'int', 'int[] ', 'out ', 'ref ', 'var'];
+                        'Object', 'string', 'int', 'int[] ', 'out ', 'ref ', 'var', 'params'];
         for (var i = 0; i < keywords.length; i++) {
             content = content.replace(new RegExp(keywords[i], 'g'), '<span style="color: blue;">' + keywords[i] + '</span>');
         }
