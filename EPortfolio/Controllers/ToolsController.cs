73a8be6d54530472f9bf5e992a491c3d3d0dbacd
Romain Parage <rparage@talentsoft.com>
2014-03-25 13:04:17 +0100

    Remove football score from this project to export it in another project

--- EPortfolio/Controllers/ToolsController.cs
+++ EPortfolio/Controllers/ToolsController.cs
@@ -1,22 +1,30 @@
-﻿using BusinessLogic.Tools;
-using EPortfolio.Models;
-using System.Net.Mail;
+﻿using System.Net.Mail;
 using System.Web.Mvc;
+using EPortfolio.Models;
 
 namespace EPortfolio.Controllers
 {
+    using System.Net;
+
     public class ToolsController : Controller
     {
         [HttpPost]
         public void SendContactMail(MailModel model)
         {
-            MailMessage mail = new MailMessage();
+            var mail = new MailMessage();
             mail.From = new MailAddress("softox@rparage.fr");
             mail.To.Add("romain.parage@gmail.com");
             mail.Subject = "Prise de contact : " + model.Email;
             mail.Body = model.Message;
 
-            Tools.SendMail(mail);
+            SendMail(mail);
+        }
+
+        public static void SendMail(MailMessage mail)
+        {
+            var smtpClient = new SmtpClient("");
+            smtpClient.Credentials = new NetworkCredential("", "");
+            smtpClient.Send(mail);
         }
     }
 }
